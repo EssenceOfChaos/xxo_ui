f81c732ef49ed1e77840cf3b1ef8c86e270ead7b
EssenceOfChaos <fjschiller@gmail.com>
2019-05-27 18:05:52 -0400

    update test configs

--- config/test.exs
+++ config/test.exs
@@ -12,7 +12,7 @@ config :logger, level: :warn
 # Configure your database
 config :xxo_ui, XxoUi.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "xxo_ui_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
